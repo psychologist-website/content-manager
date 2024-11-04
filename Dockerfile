30f9ea7b64c7e3bd20c62a7e4af7500709315693
zyabl1k <kdaniild@inbox.ru>
2024-11-04 05:34:53 +0500

    feat: full

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:20
 WORKDIR /app
 
 COPY package.json .
+COPY package-lock.json .
 RUN npm install
 
 COPY . .
--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: '0.0.0.0',
+  port: env.int('PORT', 53938),
+  url: 'http://127.0.4.38:53938',
   app: {
     keys: env.array('APP_KEYS'),
   },
